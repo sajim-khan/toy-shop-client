3a4a235f7a1489d54ba06d43dbef355d59baa66f
sajim-khan <shossain1427@gmail.com>
2023-05-19 16:57:28 +0600

    added sign up page design

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -3,7 +3,51 @@ import { Link } from "react-router-dom";
 
 const Signup = () => {
     return (
-      
+      <main className="w-full h-screen flex flex-col items-center justify-center bg-gray-50 sm:px-4">
+        <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
+          <div className="text-center">
+            <div className="mt-5 space-y-2">
+              <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+                Sign Up
+              </h3>
+            </div>
+          </div>
+          <div className="bg-white shadow p-4 py-6 space-y-8 sm:p-6 sm:rounded-lg">
+            <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+              <div>
+                <label className="font-medium">Email</label>
+                <input
+                  type="email"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                />
+              </div>
+              <div>
+                <label className="font-medium">Password</label>
+                <input
+                  type="password"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                />
+              </div>
+              <button className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+                Sign Up
+              </button>
+            </form>
+            <p className="text-center">
+              Have an account?
+              <Link to="/login">
+                <a
+                  href="javascript:void(0)"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Login
+                </a>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </main>
     );
 };
 
